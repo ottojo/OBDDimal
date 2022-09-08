832450fb48e54ec81b0357e24cd9d9395e50205f
Jonas Otto <jonas@jonasotto.com>
2022-09-08 20:18:55 +0200

    add benchmark for comparing DVO schedules

--- benches/bdd_creation.rs
+++ benches/bdd_creation.rs
@@ -1,6 +1,13 @@
 use concat_idents::concat_idents;
 use criterion::{criterion_group, Criterion};
-use obddimal::{bdd_manager::DDManager, dimacs, static_ordering};
+use obddimal::{
+    bdd_manager::{
+        dvo_schedules::{AlwaysUntilConvergence, NoDVOSchedule},
+        options::Options,
+        DDManager,
+    },
+    dimacs, static_ordering,
+};
 
 macro_rules! bdd_create_benchmark {
     ($name:ident) => {
@@ -20,9 +27,41 @@ bdd_create_benchmark!(sandwich);
 bdd_create_benchmark!(berkeleydb);
 // bdd_create_benchmark!(busybox);
 
+fn bench_compare_dvo_berkeleydb(c: &mut Criterion) {
+    let mut group = c.benchmark_group("BerkeleyDB_DVO");
+    group.sample_size(5);
+
+    group.bench_function("Never", |b| {
+        let cnf = dimacs::parse_dimacs(concat!("examples/berkeleydb.dimacs"));
+        let order = Some(static_ordering::keep(&cnf));
+        b.iter(|| {
+            DDManager::from_instance(
+                &mut cnf.clone(),
+                order.clone(),
+                Options::default().with_dvo(NoDVOSchedule::default().into()),
+            )
+        })
+    });
+
+    group.bench_function("Always until convergence", |b| {
+        let cnf = dimacs::parse_dimacs(concat!("examples/berkeleydb.dimacs"));
+        let order = Some(static_ordering::keep(&cnf));
+        b.iter(|| {
+            DDManager::from_instance(
+                &mut cnf.clone(),
+                order.clone(),
+                Options::default().with_dvo(AlwaysUntilConvergence::default().into()),
+            )
+        })
+    });
+
+    group.finish();
+}
+
 criterion_group!(
     bdd_creation,
     sandwich_create_benchmark,
     berkeleydb_create_benchmark,
     // busybox_create_benchmark,
+    bench_compare_dvo_berkeleydb,
 );
