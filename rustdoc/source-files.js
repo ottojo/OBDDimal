9d49887787db098415ded61b0594b03b9bab6518
ottojo <ottojo@users.noreply.github.com>
2022-09-23 10:38:04 +0000

    deploy: d2ebdb64940e46abf475af2515cad971ad91b0ba

--- rustdoc/source-files.js
+++ rustdoc/source-files.js
@@ -1,4 +1,5 @@
-var sourcesIndex = {};
-sourcesIndex["obddimal"] = {"name":"","dirs":[{"name":"bdd_manager","files":["dvo.rs","dvo_schedules.rs","from_dimacs.rs","graphviz.rs","hash_select.rs","mod.rs","options.rs","order.rs","reduce.rs","sat.rs","swap.rs","test.rs","util.rs"]}],"files":["bdd_node.rs","dimacs.rs","lib.rs","static_ordering.rs"]};
-sourcesIndex["testcase_generation"] = {"name":"","files":["testcase_generation.rs"]};
+var sourcesIndex = JSON.parse('{\
+"obddimal":["",[["bdd_manager",[],["dvo.rs","dvo_schedules.rs","from_dimacs.rs","graphviz.rs","hash_select.rs","mod.rs","options.rs","order.rs","reduce.rs","sat.rs","swap.rs","test.rs","util.rs"]]],["bdd_node.rs","dimacs.rs","lib.rs","static_ordering.rs"]],\
+"testcase_generation":["",[],["testcase_generation.rs"]]\
+}');
 createSourceSidebar();
