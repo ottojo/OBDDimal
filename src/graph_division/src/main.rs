02313ec5d0165c2d5a34636d3d05ad07b84aacc8
Christian Valenti <christian.valenti@uni-ulm.de>
2022-11-02 15:38:47 +0100

    Introduced Petgraph, figuring out how to build graph from dimacs

--- src/graph_division/src/main.rs
+++ src/graph_division/src/main.rs
@@ -2,11 +2,21 @@ extern crate obddimal as bdd;
 use bdd::dimacs;
 use bdd::dimacs::Instance;
 
+use std::any::type_name;
+
+use petgraph::graph::{NodeIndex, UnGraph, Graph};
+use petgraph::algo::{dijkstra, min_spanning_tree};
+use petgraph::data::FromElements;
+use petgraph::dot::{Dot, Config};
+use petgraph::Undirected;
+
 fn main() {
     let instance: Instance = dimacs::parse_dimacs("./../../examples/sandwich.dimacs");
     println!("variables = {}, clauses = {}", instance.no_variables, instance.no_clauses);
     let nodes = count_variable_occurences(&instance);
-    get_candidates(&nodes, 0.4);
+    let cand = get_candidates(nodes, 0.4);
+    let (matrix, node_clauses) = get_adjacency_matrix_and_nodes_clauses(&instance);
+    graph_from_matrix(&matrix);
 }
 
 fn count_variable_occurences(instance: &Instance) -> Vec<i32> {
@@ -29,13 +39,7 @@ fn delete_nodes(instance: &Instance, number: i32, candidates: Vec<i32>) -> (Vec<
     (deleted_nodes, affected_clauses)
 }
 
-fn count_affected_clauses(nodes: &Vec<i32>, clauses: &Vec<Vec<i32>>) -> Vec<i32> {
-    let mut affected_clauses = Vec::new();
-
-    affected_clauses
-}
-
-fn get_adjacency_matrix(instance: &Instance) -> (Vec<Vec<i32>>, Vec<Vec<i32>>) {
+fn get_adjacency_matrix_and_nodes_clauses(instance: &Instance) -> (Vec<Vec<i32>>, Vec<Vec<i32>>) {
     let n = instance.no_variables as usize;
     let mut result: Vec<Vec<i32>> = vec![vec![0; n+1]; n+1];
     let mut node_clauses: Vec<Vec<i32>> = vec![Vec::new(); n+1];
@@ -55,16 +59,41 @@ fn get_adjacency_matrix(instance: &Instance) -> (Vec<Vec<i32>>, Vec<Vec<i32>>) {
     (result, node_clauses)
 }
 
-fn get_candidates(metric: &Vec<i32>, percentage: f32) -> Vec<i32> {
+fn get_candidates(metric: Vec<i32>, percentage: f32) -> Vec<i32> {
     let mut candidates: Vec<i32> = Vec::new();
     let n = (percentage * metric.len() as f32) as i32;
-    println!("{}", n);
 
-    let metric_iterator = metric.iter().enumerate()/*.sort_by_key(|x| x.1)*/;
-    for a in metric_iterator { println!("{:?}", a)};
-    /*while candidates.len() <= n {
+    let metric_iterator = metric.iter().enumerate()/*.sort_by(|_, x| x)*/;
+    let mut tuples: Vec<(i32, i32)> = Vec::new();
+    for a in metric_iterator {
+        let (f, g) = a;
+        let x = f;
+        let y = g.clone();
+        tuples.push((x as i32, y));
+    }
+    tuples.sort_by(|a, b| a.1.cmp(&b.1));
+    println!("{:?}", tuples);
 
-    }*/
+    while candidates.len() <= n as usize {
+        if let Some((x,y)) = tuples.pop() {
+            candidates.push(x);
+            while tuples[0].1 == y {
+                if let Some((a, b)) = tuples.pop() { candidates.push(a); }
+            }
+        }
+    }
 
     candidates
+}
+
+fn graph_from_matrix(matrix: &Vec<Vec<i32>>) -> UnGraph::<i32, ()> {
+    let mut edges: Vec<(u32, u32)> = Vec::new();
+    edges.push((5,6));
+    let mut graph = Graph::new_undirected();
+    let edges2 = vec![(3,4), (4,5)];
+    graph.extend_with_edges(&[(1,2),(2,3)]);
+    graph.extend_with_edges(&edges);
+    println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+
+    graph
 }
